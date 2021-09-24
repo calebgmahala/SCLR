91dd9c134e16cc8f04c6c9ea583bfb355c27be4d
Caleb Mahala <calebgmahala@gmail.com>
2021-09-24 00:49:23 -0400

    [Resolves #1][Resolves #2] Create Base Entity
    Creates base entity and updates the world characterlist to store entities
    
    Changes
    - Added new utility types
    - Added new base entity
    - Updated World character list to store entities
    - Updated other World functions and attributes to account for entities change
    
    Testing
    - Run application in development mode
    - Use buttons in top left to interact with the World

---
+++ src/engine/utils/types.ts
@@ -0,0 +1,25 @@
+/**
+ * @packageDocumentation
+ * Utility Interfaces
+ * @module Types
+ */
+
+export interface Coordinates {
+    x: number
+    y: number
+}
+
+export interface OptionalCoordinates {
+    x?: number
+    y?: number
+}
+
+export interface Size {
+    width: number
+    height: number
+}
+
+export interface OptionalSize {
+    width?: number
+    height?: number
+}
